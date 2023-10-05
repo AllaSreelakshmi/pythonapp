e7e49a0a0b3135eeac54ddc418b1906d16112fa8
AllaSreelakshmi <144920563+AllaSreelakshmi@users.noreply.github.com>
2023-10-05 15:12:11 +0530

    Update view.py

--- view.py
+++ view.py
@@ -12,3 +12,13 @@ def home():
 if __name__ == "MY JENKINS PROJECT TASK":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
+stage('Gmail')
+{
+	steps
+	{
+		emailext body: "*${currentBuild.currentResult}:* Job Name: 
+                ${env.JOB_NAME} || Build Number: ${env.BUILD_NUMBER}\n More 
+                information at: ${env.BUILD_URL}",
+		subject: 'Declarative Pipeline Build Status',
+		to: 'sree143nani.kushi@gmail.com'
+	}
