ebaade0dd98cf07a2170b2dfd535b8b8b90a86e2
AllaSreelakshmi <144920563+AllaSreelakshmi@users.noreply.github.com>
2023-10-05 12:55:50 +0530

    Update view.py

--- view.py
+++ view.py
@@ -9,6 +9,6 @@ def home():
     return render_template('index.html')
 
 
-if __name__ == "my jenkins task":
+if __name__ == "MY JENKINS PROJECT TASK":
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
